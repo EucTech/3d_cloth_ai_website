700bffff280a7a969473d830f24474958e5f79b3
EucTech <euccreative@gmail.com>
2025-01-04 01:56:35 +0200

    created cameraRig

---
+++ client/src/canvas/CameraRIg.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+const CameraRIg = ({ children }: any) => {
+  const group = useRef(null);
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+    
+
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
+};
+
+export default CameraRIg;

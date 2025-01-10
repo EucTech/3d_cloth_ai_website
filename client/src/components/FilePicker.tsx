ef2b10cf4699a1b006e3f471b3d6ab8950a4d241
EucTech <euccreative@gmail.com>
2025-01-10 13:47:10 +0200

    updated file picker

--- client/src/components/FilePicker.tsx
+++ client/src/components/FilePicker.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
 
-const FilePicker = () => {
+import CustomButton from "./CustomButton";
+
+const FilePicker = ({ file, setFile, readFile }: any) => {
   return (
-    <div>
-      File Picker
-    </div>
-  )
-}
+    <div className="filepicker-container ">
+      <div className="flex-1 flex flex-col">
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e: any) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label ">
+          Upload File
+        </label>
 
-export default FilePicker
+        <p className="mt-2 text-gray-500 text-xs truncate">
+          {file ? file.name : "No file selected" }
+        </p>
+      </div>
+
+      <div className="mt-4 flex flex-wrap gap-3">
+        <CustomButton
+          type="outline"
+          title="Logo"
+          handleClick={() => readFile("logo")}
+          customStyles="text-xs "
+        />
+        <CustomButton
+          type="filled"
+          title="Full"
+          handleClick={() => readFile("full")}
+          customStyles="text-xs "
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FilePicker;

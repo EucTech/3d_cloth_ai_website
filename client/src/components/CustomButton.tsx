47b5dbbcf227290e95cbf195b68bc20f0d66fb66
EucTech <euccreative@gmail.com>
2025-01-10 13:46:48 +0200

    updated custom button

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import state from "../store";
 import { useSnapshot } from "valtio";
+import { color } from "framer-motion";
+
+import { getContrastingColor } from "../config/helpers";
 
 interface CustomButtonProps {
   type: string;
@@ -21,8 +24,15 @@ const CustomButton: React.FC<CustomButtonProps> = ({
     if (type === "filled") {
       return {
         backgroundColor: snap.color,
-        color: "#fff",
+        color: getContrastingColor(snap.color),
       };
+    } else if(type === "outline") {
+      return {
+        borderWidth: '1px ',
+        borderColor: snap.color,
+        color: snap.color,
+      };
+
     }
   };
 
@@ -31,6 +41,7 @@ const CustomButton: React.FC<CustomButtonProps> = ({
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
       style={generateStyle(type)}
       onClick={handleClick}
+
     >
       {title}
     </button>
